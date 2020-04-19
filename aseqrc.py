d66b4630acc08bb6e18d2673d6e3bb91ae3710a0
David Moreno <dmoreno@coralbits.com>
2020-04-19 12:18:29 +0200

    Conns are made by name, realoaded at start

--- aseqrc.py
+++ aseqrc.py
@@ -20,42 +20,89 @@
 import flask
 import sh
 import re
+import os
+import hashlib
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)-8s:%(message)s',)
+logger = logging.getLogger("aseqrc")
 # import alsaseq
 
 PATH = "."
+CONFIGFILE = os.path.expanduser("~/.config/aseqrc/current.json") if PATH == '.' else '/var/lib/aseqrc/current.json'
 app = flask.Flask(__name__, template_folder=PATH)
 PORT_INPUT = 1
 PORT_OUTPUT = 2
+PORT_ALL = 3
 RE_PARENT = re.compile(r"client (\d+): '(.*?)' \[((.*?)=(.*?))+\]$")
 RE_CHILD = re.compile(r"    (\d+) '(.*?)'$")
 RE_CONNECT_TO = re.compile(r"\tConnecting To: (.*?):(.*?)(, (.*?):(.*?))*$")
 
-hidden_in = ["Timer", "Announce"]
-hidden_out = ["Timer", "Announce"]
+
+class Config:
+    def __init__(self, filename):
+        self.config = {}
+        self.filename = filename
+        if os.path.exists(filename):
+            with open(filename) as fd:
+                self.config = json.loads(fd.read())
+        else:
+            os.makedirs(os.path.dirname(CONFIGFILE), exist_ok=True)
+            self.config = {
+                "hidden_in": ["System / Timer", "System / Announce"],
+                "hidden_out": ["System / Timer", "System / Announce"],
+                "name_to_port": {}
+            }
+
+    def get(self, key, defval=None):
+        return self.config.get(key, defval)
+
+    def __getitem__(self, name):
+        return self.config[name]
+
+    def __setitem__(self, name, value):
+        if self.config.get(name) == value:  # NOP
+            return value
+        self.config[name] = value
+        with open(self.filename, 'wt') as fd:
+            fd.write(json.dumps(self.config, indent=2))
+        return value
+
+config = Config(CONFIGFILE)
 
 
 def list_ports(type, hidden):
     if type == PORT_INPUT:
         flags = "-li"
-    else:
+    elif type == PORT_OUTPUT:
         flags = "-lo"
+    else:
+        flags = "-l"
 
+    name_to_port = config["name_to_port"]
     ret = []
     parent_id = 0
+    parent_name = ""
     for line in sh.aconnect(flags):
         m = RE_PARENT.match(line)
         if m:
-            parent_id = m.groups()[0]
+            parent_id = m.group(1)
+            parent_name = m.group(2)
         m = RE_CHILD.match(line)
         if m:
-            label = m.groups()[1].strip()
+            label = m.group(2).strip()
+            label = f"{parent_name} / {label}"
             if label in hidden:
                 continue
-            port_id = m.groups()[0]
+            port_id = m.group(1)
+            port = f"{parent_id}:{port_id}"
             ret.append({
-                "id": "%s:%s" % (parent_id, port_id),
-                "label": label
+                "port": port,
+                "label": label,
+                "id": label,
             })
+            name_to_port[label] = port
     return ret
 
 
@@ -63,22 +110,67 @@ def list_connections():
     ret = {}
     frms = []
     parent_id = 0
+    parent_name = ""
     port_id = 0
+    port_to_name = {}
     for line in sh.aconnect("-l"):
         m = RE_PARENT.match(line)
         if m:
-            parent_id = m.groups(1)[0]
+            parent_id = m.group(1)
+            parent_name = m.group(2)
         m = RE_CHILD.match(line)
         if m:
-            port_id = m.groups(1)[0]
+            port_id = m.group(1)
+            label = m.group(2).strip()
             frms = []
-            ret["%s:%s" % (parent_id, port_id)] = frms
+            name = f"{parent_name} / {label}"
+            if name in config["hidden_in"]:
+                continue
+            ret[name] = frms
+            port = f"{parent_id}:{port_id}"
+            port_to_name[port] = name
         m = RE_CONNECT_TO.match(line)
         if m:
             for idx in range(0, len(m.groups()), 3):
-                frms.append("%s:%s" % (m.groups()[idx], m.groups()[idx + 1]))
+                print(line, m.groups())
+                if not m.groups()[idx]:
+                    continue
+                port = "%s:%s" % (m.groups()[idx], m.groups()[idx + 1])
+                name = port_to_name.get(port)
+                if name not in config["hidden_in"]:
+                    frms.append(name)
+
+    config["connections"] = ret
 
     return ret
+
+def connect(from_, to_):
+    logger.info("Connect <%s> to <%s>", from_, to_)
+    name_to_port = config["name_to_port"]
+    print(name_to_port, from_, to_)
+    if from_ not in name_to_port:
+        logger.debug("Unknown port name: %s", from_)
+        return
+    if to_ not in name_to_port:
+        logger.debug("Unknown port name: %s", to_)
+        return
+    try:
+        sh.aconnect(name_to_port[from_], name_to_port[to_])
+    except sh.ErrorReturnCode as e:
+        errors = ["Could not disconnect", str(e)]
+
+
+def disconnect(from_, to_):
+    logger.info("Disconnect <%s> to <%s>", from_, to_)
+    name_to_port = config["name_to_port"]
+    if not from_ in name_to_port:
+        logger.debug("Unknonw port name: %s", from_)
+    if not to_ in name_to_port:
+        logger.debug("Unknonw port name: %s", to_)
+    try:
+        sh.aconnect("-d", name_to_port[from_], name_to_port[to_])
+    except sh.ErrorReturnCode as e:
+        errors = ["Could not disconnect", str(e)]
 
 
 @app.route("/sw.js", methods=["GET"])
@@ -108,30 +200,26 @@ def index_html():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    global hidden_in, hidden_out
     errors = []
     if flask.request.method == "POST":
         data = flask.request.form
         if 'hide_in' in data:
-            hidden_in.append(data["hide_in"])
+            config["hidden_in"].append(data["hide_in"])
         elif 'hide_out' in data:
-            hidden_out.append(data["hide_out"])
+            config["hidden_out"].append(data["hide_out"])
         elif 'show_in' in data:
-            hidden_in = [x for x in hidden_in if x != data["show_in"]]
+            config["hidden_in"] = [x for x in config["hidden_in"] if x != data["show_in"]]
         elif 'show_out' in data:
-            hidden_out = [x for x in hidden_out if x != data["show_out"]]
-        elif 'conn' in data:
-            try:
-                if 'on' not in data:
-                    sh.aconnect("-d", *data["conn"].split())
-                elif 'on' in data:
-                    sh.aconnect(*data["conn"].split())
-            except sh.ErrorReturnCode as e:
-                errors = ["Could not connect/disconnect", str(e)]
+            config["hidden_out"] = [x for x in config["hidden_out"] if x != data["show_out"]]
+        elif 'from' in data:
+            if 'on' in data:
+                connect(data["from"], data["to"])
+            else:
+                disconnect(data["from"], data["to"])
         return flask.redirect("/")
 
-    input_ports = list_ports(PORT_INPUT, hidden_in)
-    output_ports = list_ports(PORT_OUTPUT, hidden_out)
+    input_ports = list_ports(PORT_INPUT, config["hidden_in"])
+    output_ports = list_ports(PORT_OUTPUT, config["hidden_out"])
 
     connections = list_connections()
 
@@ -141,10 +229,33 @@ def index():
         outputs=output_ports,
         connections=connections,
         errors=errors,
-        hidden_in=hidden_in,
-        hidden_out=hidden_out,
+        hidden_in=config["hidden_in"],
+        hidden_out=config["hidden_out"],
     )
 
+@app.route("/status", methods=["GET", "POST"])
+def status():
+    print(config)
+    input_ports = list_ports(PORT_INPUT, config["hidden_in"])
+    output_ports = list_ports(PORT_OUTPUT, config["hidden_out"])
+
+    connections = list_connections()
+
+    return flask.jsonify({
+        "inputs": input_ports,
+        "outputs": output_ports,
+        "connections": connections,
+        "hidden_in": config["hidden_in"],
+        "hidden_out": config["hidden_out"],
+    })
+
+def setup():
+    ports = list_ports(PORT_ALL, [])
+
+    for from_, tos_ in config["connections"].items():
+        for to_ in tos_:
+            connect(from_, to_)
 
 if __name__ == '__main__':
+    setup()
     app.run(debug=True, host="0.0.0.0")
