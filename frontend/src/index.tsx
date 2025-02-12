cd8e7cc1502151c0157e956475a6bdea07ab7aca
David Moreno <dmoreno@coralbits.com>
2025-02-12 21:29:03 +0100

    Updated react and other packages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById("main"),
-)
+const root = ReactDOM.createRoot(document.getElementById("main"));
+root.render(<App />);
